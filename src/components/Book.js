7fb162f733142f789e39d0c60d158cc95d8f8053
Abhishek Singh <abhi.devwork@outlook.com>
2018-11-16 21:04:32 +0530

    add join() method on author names

--- src/components/Book.js
+++ src/components/Book.js
@@ -19,7 +19,7 @@ class Book extends React.Component {
                             </div>
                           </div>
                           <div className="book-title">{this.props.book.title ? this.props.book.title : "No Title"}</div>
-                          <div className="book-authors">{this.props.book.authors ? this.props.book.authors : "No Author"}</div>
+                          <div className="book-authors">{this.props.book.authors ? this.props.book.authors.join(', ') : "No Author"}</div>
                         </div>
                       </li>
             </div>
--- src/views/Search.js
+++ src/views/Search.js
@@ -2,6 +2,7 @@ import React from 'react';
 import * as BooksAPI from '../BooksAPI';
 import Book from '../components/Book';
 import {Link} from 'react-router-dom';
+import {DebounceInput} from 'react-debounce-input';
 
 class Search extends React.Component {
 
